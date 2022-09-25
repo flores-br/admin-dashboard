f01eaa05f3b6596db0ba9339ad087ba2d7a361c9
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-25 13:31:18 -0400

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,16 @@ const Container = styled.nav`
       font-size: 2rem;
     }
   }
+
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem;
+    margin-bottom: 0;
+    .timeline {
+      gap: 1rem;
+    }
+  }
 `;
 
 const Navbar = () => {
