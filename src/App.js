d69ad7363c4afac94454901f3d12991553852bba
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-25 13:28:06 -0400

    app.js responsive

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ const Container = styled.div`
   grid-template-columns: 1fr 12fr 4fr;
   min-height: 100vh;
   height: max-content;
+
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    grid-template-columns: 1fr;
+    height: max-content;
+  }
 `;
 
 const App = () => {
