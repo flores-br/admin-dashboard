964dffb9664f477c66ee6548121c8dda3176193a
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-25 12:21:45 -0400

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+
+// components
+import Sidebar from './components/Sidebar';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 12fr 4fr;
+  min-height: 100vh;
+  height: max-content;
+`;
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container>
+      <Sidebar />
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+
+// icons
+import { RiNeteaseCloudMusicLine } from 'react-icons/ri';
+import { HiOutlineMusicNote } from 'react-icons/hi';
+import { BiGroup, BiBell } from 'react-icons/bi';
+import { AiOutlineDollarCircle } from 'react-icons/ai';
+import { SiAnalogue } from 'react-icons/si';
+import { FiHelpCircle } from 'react-icons/fi';
+
+const Container = styled.section`
+  background-color: var(--dark-background-color);
+  height: 100%;
+  width: max-content;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  .help {
+    svg {
+      transition: 300ms ease-in-out;
+      cursor: pointer;
+      color: white;
+    }
+    &:hover {
+      svg {
+        color: var(--primary-color);
+      }
+    }
+  }
+  svg {
+    color: var(--primary-color);
+    font-size: 1.5rem;
+  }
+  .brand {
+    svg {
+      cursor: pointer;
+      font-size: 2.5rem;
+    }
+  }
+  .links {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    list-style-type: none;
+    li {
+      border-radius: 1rem;
+      padding: 0.5rem;
+      transition: 300ms ease-in-out;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      svg {
+        color: white;
+      }
+      &:hover {
+        box-shadow: 0 0 60px 8px var(--primary-color);
+        svg {
+          color: var(--primary-color);
+        }
+      }
+    }
+    .selected {
+      box-shadow: 0 0 60px 8px var(--primary-color);
+      svg {
+        color: var(--primary-color);
+        background-color: transparent;
+      }
+    }
+  }
+`;
+
+const Sidebar = () => {
+  return (
+    <Container id='sidebar'>
+      <div className='brand'>
+        <RiNeteaseCloudMusicLine />
+      </div>
+      <ul className='links'>
+        <li className='selected'>
+          <SiAnalogue />
+        </li>
+        <li>
+          <HiOutlineMusicNote />
+        </li>
+        <li>
+          <BiBell />
+        </li>
+        <li>
+          <AiOutlineDollarCircle />
+        </li>
+        <li>
+          <BiGroup />
+        </li>
+      </ul>
+      <div className='help'>
+        <FiHelpCircle />
+      </div>
+    </Container>
+  );
+};
+
+export default Sidebar;
