bea3ec7aa84a4d6bc30d21f4983e9f9e0b4b4a22
Bryan-PC2 <bryanflores12345@gmail.com>
2022-09-25 13:16:13 -0400

    active listeners component

--- src/components/ActiveListeners.jsx
+++ src/components/ActiveListeners.jsx
@@ -1,5 +1,122 @@
+import styled from 'styled-components';
+import { AreaChart, Area, ResponsiveContainer } from 'recharts';
+
+import { countries, countryData as data } from '../dummyData';
+
+const Container = styled.section`
+  border-bottom: 0.1rem solid #242424;
+  color: white;
+  .title-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 100%;
+    padding-top: 1rem;
+    .title {
+      h4 {
+        font-size: 0.8rem;
+        margin-bottom: 0.2rem;
+      }
+      h1 {
+        font-size: 1.5rem;
+        letter-spacing: 0.2rem;
+      }
+    }
+    .chart {
+      position: relative;
+      .percent {
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: var(--primary-color);
+        font-size: 0.8rem;
+      }
+      height: 4rem;
+      width: 100%;
+    }
+  }
+  .active {
+    max-height: 11rem;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding-right: 1.5rem;
+    margin: 1rem 0;
+    ::-webkit-scrollbar {
+      width: 0.2rem;
+      background-color: #6e6e6ec3;
+      &-thumb {
+        background-color: #b8b8b8;
+      }
+    }
+    h5 {
+      font-weight: 100;
+    }
+    .country {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .name {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+        img {
+          height: 2rem;
+          border-radius: 2rem;
+        }
+      }
+    }
+  }
+`;
+
 const ActiveListeners = () => {
-  return <div>ActiveListeners</div>;
+  return (
+    <Container>
+      <div className='title-container'>
+        <div className='title'>
+          <h4>Current Listeners</h4>
+          <h1>1,102</h1>
+        </div>
+        <div className='chart'>
+          <div className='percent'>+11.3%</div>
+          <ResponsiveContainer width='100%' height='100%'>
+            <AreaChart width={500} height={400} data={data}>
+              <defs>
+                <linearGradient id='colorview' x1='0' y1='0' x2='0' y2='1'>
+                  <stop
+                    offset='10%'
+                    stopColor='var(--primary-color)'
+                    stopOpacity={0.4}
+                  />
+                  <stop offset='100%' stopColor='#000000ff' stopOpacity={0.2} />
+                </linearGradient>
+              </defs>
+              <Area
+                type='monotone'
+                dataKey='data'
+                stroke='var(--primary-color)'
+                strokeWidth={2}
+                fill='url(#colorview)'
+                animationBegin={500}
+                animationDuration={1000}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+      <div className='active'>
+        {countries.map((data, index) => (
+          <div className='country' key={index}>
+            <div className='name'>
+              <img src={data.image} alt={data.name} />
+              <h5>{data.name}</h5>
+            </div>
+            <h5>{data.amount}</h5>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default ActiveListeners;
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,14 +1,24 @@
+// logos
 import logo1 from './assets/logo1.png';
 import logo2 from './assets/logo2.png';
 import logo3 from './assets/logo3.png';
 import logo4 from './assets/logo4.png';
 
+// music
 import music1 from './assets/music1.jpeg';
 import music2 from './assets/music2.jpeg';
 import music3 from './assets/music3.jpeg';
 import music4 from './assets/music4.jpeg';
 import music5 from './assets/music5.jpeg';
 import music6 from './assets/music6.jpeg';
+
+// countries
+import canada from './assets/canada.png';
+import united from './assets/united.png';
+import austallia from './assets/austallia.png';
+import india from './assets/india.png';
+import france from './assets/france.png';
+import kingdom from './assets/kingdom.png';
 
 export const data = [
   {
@@ -258,5 +268,233 @@ export const musicData = [
     title: 'Echo',
     plays: 25,
     image: music6,
+  },
+];
+
+export const countries = [
+  {
+    name: 'United States',
+    amount: 124,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 52,
+    image: canada,
+  },
+  {
+    name: 'France',
+    amount: 64,
+    image: france,
+  },
+  {
+    name: 'United States',
+    amount: 152,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 10,
+    image: canada,
+  },
+  {
+    name: 'India',
+    amount: 64,
+    image: india,
+  },
+  {
+    name: 'United States',
+    amount: 24,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 10,
+    image: canada,
+  },
+  {
+    name: 'India',
+    amount: 64,
+    image: india,
+  },
+  {
+    name: 'United States',
+    amount: 24,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 10,
+    image: canada,
+  },
+  {
+    name: 'India',
+    amount: 64,
+    image: india,
+  },
+  {
+    name: 'United States',
+    amount: 24,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 10,
+    image: canada,
+  },
+  {
+    name: 'Australia',
+    amount: 4,
+    image: austallia,
+  },
+  {
+    name: 'France',
+    amount: 22,
+    image: france,
+  },
+  {
+    name: 'India',
+    amount: 64,
+    image: india,
+  },
+  {
+    name: 'United States',
+    amount: 24,
+    image: united,
+  },
+  {
+    name: 'United Kingdom',
+    amount: 13,
+    image: kingdom,
+  },
+  {
+    name: 'Canada',
+    amount: 10,
+    image: canada,
+  },
+  {
+    name: 'Australia',
+    amount: 4,
+    image: austallia,
+  },
+  {
+    name: 'France',
+    amount: 22,
+    image: france,
+  },
+];
+
+export const countryData = [
+  {
+    data: 3800,
+  },
+  {
+    data: 3900,
+  },
+  {
+    data: 4000,
+  },
+  {
+    data: 4100,
+  },
+  {
+    data: 4200,
+  },
+  {
+    data: 4300,
+  },
+  {
+    data: 4400,
+  },
+  {
+    data: 4500,
+  },
+  {
+    data: 4600,
+  },
+  {
+    data: 4700,
+  },
+  {
+    data: 4800,
+  },
+  {
+    data: 4900,
+  },
+  {
+    data: 5100,
+  },
+  {
+    data: 5200,
+  },
+  {
+    data: 5300,
+  },
+  {
+    data: 5400,
+  },
+  {
+    data: 5500,
+  },
+  {
+    data: 5600,
+  },
+  {
+    data: 6000,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6580,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6700,
+  },
+  {
+    data: 7000,
+  },
+  {
+    data: 7300,
+  },
+  {
+    data: 7500,
+  },
+  {
+    data: 7700,
   },
 ];
